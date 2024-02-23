f38797d3cc8ccacbd03d582fe90131f60834d324
Saverio <svscagn@gmail.com>
2024-02-24 00:35:07 +0100

    pass basic csv tests

--- csv/src/index.ts
+++ csv/src/index.ts
@@ -1,12 +1,14 @@
 import { DELIMITER } from "@consts";
-import { Entry } from "@types";
+import { Entry, Value } from "@types";
 import {
   existsSync,
   unlinkSync,
   writeFileSync,
   PathLike,
-  createWriteStream
+  createWriteStream,
+  createReadStream
 } from "fs";
+import split2 from "split2";
 
 interface CSVOptions<T extends string> {
   /**
@@ -32,25 +34,87 @@ class CSV<T extends string> {
 
   public constructor({ src, header, deletePrevious }: CSVOptions<T>) {
     this.src = src;
-    this.header = header;
+    this.header = this.stripHeader(header);
 
     this.init(src, !!deletePrevious);
   }
 
   private init(src: PathLike, deletePrevious: boolean) {
     if (existsSync(src) && deletePrevious) {
+      console.log("Deleting previous file...");
       unlinkSync(src);
     }
 
     writeFileSync(src, this.header.join(DELIMITER) + "\n");
   }
 
+  /**
+   * Function to strip the headers of any prefixes or suffixes used for typing.
+   * @param headers The array of headers to use when parsing the CSV file.
+   * @private
+   */
+  private stripHeader(headers: T[]): T[] {
+    if (!headers || headers.length === 0) return [];
+
+    return headers.map(h => h.replace(/^(n|b|s):|\?$/g, "").trim()) as T[];
+  }
+
+  public async read(options: { limit?: number } = {}) {
+    let data = [] as Entry<T>[];
+
+    let stream = createReadStream(this.src, { encoding: "utf-8" });
+
+    let limit = options.limit || Infinity;
+    let first = true;
+
+    stream.pipe(split2()).on("data", (line: string) => {
+      if (first) {
+        first = false;
+        return;
+      }
+
+      if (data.length >= limit) {
+        stream.destroy();
+        return;
+      }
+
+      let values = line.split(DELIMITER);
+      let entry = {} as Record<string, unknown>;
+
+      for (let i = 0; i < values.length; i++) {
+        let value = values[i];
+        let valueLower = value.toLowerCase();
+        let parsed: Value;
+
+        if (!isNaN(+value)) {
+          parsed = +value;
+        } else if (valueLower === "true" || valueLower === "false") {
+          parsed = valueLower === "true";
+        } else if (["null", "undefined", ""].includes(valueLower)) {
+          parsed = null;
+        } else {
+          parsed = value;
+        }
+
+        entry[this.header[i]] = parsed;
+      }
+
+      data.push(entry as Entry<T>);
+    });
+
+    return new Promise<Entry<T>[]>((res, rej) => {
+      stream.on("close", () => res(data));
+      stream.on("error", err => rej(err));
+    });
+  }
+
   public async write(data: Entry<T>[]) {
     let stream = createWriteStream(this.src, { flags: "a" });
 
     for (let i = 0; i < data.length; i++) {
-      let entry = data[i];
-      let values = this.header.map(header => entry[header as unknown as keyof Entry<T>]).join(DELIMITER);
+      let entry = data[i] as Record<string, unknown>;
+      let values = this.header.map(header => entry[header]).join(DELIMITER);
+
       stream.write(values + "\n");
     }
 
---
+++ csv/test/csv.test.ts
@@ -0,0 +1,94 @@
+import { CSV } from "../dist";
+import path from "path";
+import { test, it, expect, afterAll } from "vitest";
+import { faker } from "@faker-js/faker";
+import { describe } from "node:test";
+
+describe("csv", () => {
+  test("read", async () => {
+    let csv = new CSV({
+      src: path.join(__dirname, "read.csv"),
+      header: ["s:name", "n:age", "s:sex", "s:hair_color", "s:zodiac_sign"]
+    });
+
+    let data = Array.from({ length: 100 }, () => ({
+      name: faker.person.firstName(),
+      sex: faker.person.sex(),
+      hair_color: faker.color.rgb(),
+      age: faker.number.int({ min: 1, max: 100 }),
+      zodiac_sign: faker.person.zodiacSign()
+    }));
+
+    await csv.write(data);
+
+    let read = await csv.read();
+
+    expect(read).toEqual(data);
+  });
+
+  test("basic write", async () => {
+    let csv = new CSV({
+      src: path.join(__dirname, "basic-write.csv"),
+      header: ["s:name", "n:age", "s:sex", "s:hair_color", "s:zodiac_sign"]
+    });
+
+    let data = Array.from({ length: 100 }, () => ({
+      name: faker.person.firstName(),
+      sex: faker.person.sex(),
+      hair_color: faker.color.rgb(),
+      age: faker.number.int({ min: 1, max: 100 }),
+      zodiac_sign: faker.person.zodiacSign()
+    }));
+
+    await csv.write(data);
+
+    let read = await csv.read();
+
+    expect(read).toEqual(data);
+  });
+
+  test(
+    "write large",
+    async () => {
+      let csv = new CSV({
+        src: path.join(__dirname, "write-large.csv"),
+        header: ["s:name", "n:age", "s:sex", "s:hair_color", "s:zodiac_sign"]
+      });
+
+      let data = Array.from({ length: 1000000 }, () => ({
+        name: faker.person.firstName(),
+        sex: faker.person.sex(),
+        hair_color: faker.color.rgb(),
+        age: faker.number.int({ min: 1, max: 100 }),
+        zodiac_sign: faker.person.zodiacSign()
+      }));
+
+      await csv.write(data);
+
+      let data2 = Array.from({ length: 1000000 }, () => ({
+        name: faker.person.firstName(),
+        sex: faker.person.sex(),
+        hair_color: faker.color.rgb(),
+        age: faker.number.int({ min: 1, max: 100 }),
+        zodiac_sign: faker.person.zodiacSign()
+      }));
+
+      await csv.write(data2);
+
+      let read = await csv.read();
+
+      expect(read).toEqual(data.concat(data2));
+    },
+    { timeout: -1 }
+  );
+
+  afterAll(() => {
+    let files = ["read.csv", "basic-write.csv", "write-large.csv"].map(file =>
+      path.join(__dirname, file)
+    );
+
+    for (let file of files) {
+      require("fs").unlinkSync(file);
+    }
+  });
+});
