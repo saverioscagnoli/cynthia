1fea99c30de0d5e02da2a9e43fb39d274fcb631a
Saverio Scagnoli <svscagn@gmail.com>
2026-06-04 13:54:05 +0200

    add event names

--- ds/types/message.go
+++ ds/types/message.go
@@ -1,26 +1,26 @@
 package ds
 
 type Message struct {
-	ID              string            `json:"id"`
-	ChannelID       string            `json:"channel_id"`
-	Author          User              `json:"author"`
-	Content         string            `json:"content"`
-	Timestamp       string            `json:"timestamp"`
-	EditedTimestamp string            `json:"edited_timestamp"`
-	TTS             bool              `json:"tts"`
-	MentionEveryone bool              `json:"mention_everyone"`
-	Mentions        []User            `json:"mentions"`
-	MentionRoles    []string          `json:"mention_roles"`
-	MentionChannels []ChannelMentions `json:"mention_channels"`
-	Attachments     []Attachment      `json:"attachments"`
-	Embeds          []Embed           `json:"embeds"`
-	Reactions       []Reaction        `json:"reactions"`
-	Nonce           *string           `json:"nonce"`
-	Pinned          bool              `json:"pinned"`
-	WebhookID       *Snowflake        `json:"webhook_id"`
-	Type            int               `json:"type"`
-	Activity        *MessageActivity  `json:"activity"`
-	// TODO: Application     *Application      `json:"application"`
+	ID                   string                      `json:"id"`
+	ChannelID            string                      `json:"channel_id"`
+	Author               User                        `json:"author"`
+	Content              string                      `json:"content"`
+	Timestamp            string                      `json:"timestamp"`
+	EditedTimestamp      string                      `json:"edited_timestamp"`
+	TTS                  bool                        `json:"tts"`
+	MentionEveryone      bool                        `json:"mention_everyone"`
+	Mentions             []User                      `json:"mentions"`
+	MentionRoles         []string                    `json:"mention_roles"`
+	MentionChannels      []ChannelMentions           `json:"mention_channels"`
+	Attachments          []Attachment                `json:"attachments"`
+	Embeds               []Embed                     `json:"embeds"`
+	Reactions            []Reaction                  `json:"reactions"`
+	Nonce                *string                     `json:"nonce"`
+	Pinned               bool                        `json:"pinned"`
+	WebhookID            *Snowflake                  `json:"webhook_id"`
+	Type                 int                         `json:"type"`
+	Activity             *MessageActivity            `json:"activity"`
+	Application          *Application                `json:"application"`
 	ApplicationID        *Snowflake                  `json:"application_id"`
 	Flags                *int                        `json:"flags"`
 	MessageReference     *MessageReference           `json:"message_reference"`
