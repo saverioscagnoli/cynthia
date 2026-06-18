4a6140bca8f7d4e9422dbbeeccfe7c594f3aac50
saverioscagnoli <svscagn@gmail.com>
2026-06-19 00:10:41 +0200

    ping

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"cynthia/ds"
+	"fmt"
 	"log/slog"
 )
 
@@ -17,6 +18,17 @@ func main() {
 
 	ds.On(app.ds, ds.EventReady, func(c *ds.Client, a *ds.Ready) {
 		slog.Info("Ready event received", "version", a.Version)
+	})
+
+	ds.On(app.ds, ds.EventMessageCreate, func(c *ds.Client, msg *ds.MessageCreate) {
+		if msg.Content == "!ping" {
+			_, err := c.Api.SendMessageText(msg.ChannelID, fmt.Sprintf("Pong! `%dms`", c.Latency().Milliseconds()))
+
+			if err != nil {
+				slog.Error("Error in message create", "err", err)
+				return
+			}
+		}
 	})
 
 	app.ds.Login()
