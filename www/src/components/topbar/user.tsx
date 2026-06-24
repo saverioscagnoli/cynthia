fc9807a8de24129d7fd2db233831ba4e3ed14e31
saverioscagnoli <svscagn@gmail.com>
2026-06-24 02:07:53 +0200

    go home on logout

--- www/src/components/topbar/user.tsx
+++ www/src/components/topbar/user.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router";
 import { Avatar, Button } from "~/components/ui";
 import { cn, dsAvatar } from "~/lib/utils";
 import { useAuth } from "~/contexts/auth";
@@ -6,6 +7,12 @@ import { ProfileMenu } from "./profile-menu";
 const UserDisplay = () => {
   const { loggedUser, logout } = useAuth();
   const user = loggedUser!;
+  const nav = useNavigate();
+
+  const onLogout = () => {
+    logout();
+    nav("/");
+  };
 
   return (
     <div className={cn("w-fit", "flex items-center gap-4")}>
@@ -15,7 +22,7 @@ const UserDisplay = () => {
           <p>{user.username}</p>
         </div>
       </ProfileMenu>
-      <Button variant="soft" colorScheme="red" onClick={logout}>
+      <Button variant="soft" colorScheme="red" onClick={onLogout}>
         Logout
       </Button>
     </div>
